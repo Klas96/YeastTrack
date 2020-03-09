f557d65d47801d07a1fab1864f5f6cca0af17c73
klas96 <klas0holmgren@gmail.com>
2020-03-09 14:07:30 +0100

    Flor and frame trackbars

--- main.py
+++ main.py
@@ -4,27 +4,73 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+#global variables
+currentFrame = 1
+currentBlend = 0
+
+def loadChannels():
+    #Get File Paths
+    filePathC1 = "/home/klas/Documents/Chalmers/ExamensArbete/YeastTrack/VideoData/Experiment13h_050619/vidP1C1Z4.avi"
+    filePathC2 = "/home/klas/Documents/Chalmers/ExamensArbete/YeastTrack/VideoData/Experiment13h_050619/vidP1C2Z1.avi"
+    #Get video Capture
+    vidC1 = cv2.VideoCapture(filePathC1)
+    vidC2 = cv2.VideoCapture(filePathC2)
+    return(vidC1,vidC2)
+
+def getVideoFrame():
+    global currentFrame
+    vidC1.set(cv2.CAP_PROP_POS_FRAMES, currentFrame)
+    gotFrame,vidFrame = vidC1.read()
+    return(vidFrame)
+
+def getFlorFrame():
+    global currentFrame
+    vidC2.set(cv2.CAP_PROP_POS_FRAMES, currentFrame)
+    gotFrame,floFrame = vidC2.read()
+    return(floFrame)
+
+def incFloIntens(frame,intens):
+    
+
+
+def blendFrames(frame1,frame2):
+    global currentBlend
+
+    blendedFrame = cv2.add(frame1*currentBlend, frame2)
+    return(blendedFrame)
+
+def updateFrame():
+    vidFrame = getVideoFrame()
+    floFrame = getFlorFrame()
+    finalFrame = blendFrames(floFrame,vidFrame)
+    #finalFrame = floFrame
+    cv2.imshow('CellTracker', finalFrame)
+    print("updateFrame: ")
+    return()
+
+
 def changeFrame(frameNum):
-     vidC1.set(cv2.CAP_PROP_POS_FRAMES, frameNum)
-     gotFrame,frame = vidC1.read()
-     cv2.imshow('CellTracker', frame)
+    global currentFrame
+    currentFrame = frameNum
+    updateFrame()
 
-#Get File Paths
-filePathC1 = "/home/klas/Documents/Chalmers/ExamensArbete/YeastTrack/VideoData/Experiment13h_050619/vidC1Z4.avi"
-filePathC2 = "/home/klas/Documents/Chalmers/ExamensArbete/YeastTrack/VideoData/Experiment13h_050619/vidC2Z2.avi"
+#Change Between Florecent And Video Channel
+def changeChanell(division):
+    global currentBlend
+    currentBlend = division
+    updateFrame()
 
-vidC1 = cv2.VideoCapture(filePathC1)
-vidC2 = cv2.VideoCapture(filePathC2)
+#Main
 
-sliderPos = 0
+vidC1,vidC2 = loadChannels()
 
 cv2.namedWindow('CellTracker')
 
+sliderPos = 0
 numFrames = int(vidC1.get(cv2.CAP_PROP_FRAME_COUNT))
 
-print(numFrames)
-
 cv2.createTrackbar("Frame",'CellTracker',sliderPos,numFrames,changeFrame)
+cv2.createTrackbar("Channel",'CellTracker',0,100,changeChanell)
 
 while(True):
 
