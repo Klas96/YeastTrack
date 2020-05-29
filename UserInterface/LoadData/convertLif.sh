6a1149dfa7d51cbae1c0a10800585bd1581f7d54
klas96 <klas0holmgren@gmail.com>
2020-05-29 15:41:10 +0200

    Added all

--- UserInterface/LoadData/convertLif.sh
+++
@@ -1,2 +0,0 @@
-#./bftools/bfconvert -series 3 -channel 0 -z 0 ./VideoData/Experiment13h_050619/Experiment13h_050619.lif ./VideoData/WorkingData/working.jpg
-./bftools/bfconvert -nolookup -series 3 -channel 0 -z 0 ./VideoData/Experiment13h_050619/Experiment13h_050619.lif ./VideoData/WorkingData/working.tif
